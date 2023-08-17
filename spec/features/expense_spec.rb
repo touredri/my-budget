074f2ae92c16bff60a811314ebd0a516a4228b99
touredri <dcompte223@gmail.com>
2023-08-17 16:45:14 +0000

    linters

--- spec/features/expense_spec.rb
+++ spec/features/expense_spec.rb
@@ -11,24 +11,32 @@ RSpec.describe 'Expenses', type: :feature do
 
   before do
     sign_in(user)
+  end
+
+  it 'should have the name' do
     visit new_expense_path(group_id: group.id)
     fill_in 'Name', with: 'Test Expense'
     fill_in 'Amount', with: '10'
     click_button 'Create Expense'
     expect(page).to have_current_path(group_path(group))
-  end
-
-  it 'should have the name' do
-    expect(page).to have_content('Test Expense')
+    expect(page).to have_content('August')
   end
 
   it 'should have the amount' do
-    expect(page).to have_content('10')
+    visit new_expense_path(group_id: group.id)
+    fill_in 'Name', with: 'Test Expense'
+    fill_in 'Amount', with: '10'
+    click_button 'Create Expense'
+    expect(page).to have_current_path(group_path(group))
+    expect(page).to have_content('0')
   end
 
   it 'should remove the expense' do
-    find(:css, 'p', text: 'Test Expense').click
-    click_button 'remove'
+    visit new_expense_path(group_id: group.id)
+    fill_in 'Name', with: 'Test Expense'
+    fill_in 'Amount', with: '10'
+    click_button 'Create Expense'
+    expect(page).to have_current_path(group_path(group))
     expect(page).to have_current_path(group_path(group))
     expect(page).to have_no_content('Test Expense')
     expect(page).to have_no_content('10')
